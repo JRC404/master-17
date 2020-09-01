3daaba6a372f0927c63d02e30e17d2093c05184f
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-01 13:35:12 +0100

    first session on variables

---
+++ variables/index.js
@@ -0,0 +1,37 @@
+//* created a variables folder with an index.js inside of it
+//* index or app.js will be your starter file 99 times out of 100
+//* installed extensions: 
+//* live server and live share
+
+//* variable: storage places for our code
+//* storing a value to use it again later on...
+
+let myName = "Jacob Reilly-Cooper"; // string variable
+//* ; = end of a statement
+let myAge = 55; // number variable
+let myPlaceOfWork = "Code Nation"; // string variable
+let happy = true; // boolean variable
+let car = "Reliant Robin";
+let carAge = 20;
+
+
+// TODO: full sentence with your name / age or other in a console.log
+console.log("Hello, my name is " + myName + " and I am " + myAge + " years old."); 
+// old fashioned way
+myName = "Dean Singleton"
+console.log(`Hello, my name is ${myName} and I am ${myAge} years old.`) // new fashioned way
+
+
+// **********************************
+// console.log("My car is a " + car + ". Do you like it?"); // logs the variable myName to the console
+// string iterpolation
+
+// TODO: create four variables with your details OR made up details
+
+//* let Jacob Reilly-Cooper be stored in a box called myName
+//* let is creating our variable
+//* myName is the name of the variable
+//* "value of the variable"
+
+// console.log("Hello, my name is Blaze Reilly-Cooper, I am 56 years old. My car is Fiat 500, it is 1 years old and I am happy.")
+// console.log("Hello, my name is Blaze Reilly-Cooper, I am 56 years old. My car is Fiat 500, it is 1 years old and I am happy.")
