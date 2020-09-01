4b3b78155cd1e9e66ddbcfebf4856648f9786078
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-01 16:30:48 +0100

    cash machine variables added

---
+++ variables/cashmachine.js
@@ -0,0 +1,76 @@
+//* what can you do at a cash machine?
+//* count the features...
+
+//* string, number, boolean, null, undefined
+
+// withdrawing cash
+let cardActive = true; // true or false
+let accountNumber = "123456789"; // number // string
+let pinNumber = "1234"; // number // string
+let accountBalance = 100; // number
+let amountToWithdraw = 90; // number
+
+//* artithmetic operators
+// console.log(accountBalance - amountToWithdraw);
+// console.log(accountBalance * amountToWithdraw);
+// console.log(accountBalance / amountToWithdraw);
+// console.log(accountBalance + amountToWithdraw);
+// console.log(accountBalance ++); // incrementation +1
+// console.log(accountBalance --); // decrementation -1
+
+//* asssignment operators
+// accountBalance = amountToWithdraw // making the left = the right - storing the value in the left variable from the right
+// console.log(accountBalance)
+// accountBalance += amountToWithdraw // short hand
+// accountBalance -= amountToWithdraw // short hand
+// accountBalance *= amountToWithdraw // short hand
+// accountBalance /= amountToWithdraw // short hand
+// console.log(accountBalance)
+// accountBalance = accountBalance + amountToWithdraw // long hand
+
+// console.log(accountBalance -= amountToWithdraw);
+//         //      100        -    90          =   10
+// console.log(accountBalance -= amountToWithdraw);
+//         //      10        -    90          =   -80
+// console.log(accountBalance -= amountToWithdraw);
+// // accountBalance = accountBalance - amountToWithdraw
+// console.log(accountBalance);
+//         //      
+
+// TODO: create two variables that a store a number...
+// ? console.log(accountBalance - amountToWithdraw);
+// TODO: +
+// TODO: -
+// TODO: /
+// TODO: *
+// TODO: += 
+// TODO: -= 
+// TODO: *= 
+// TODO: /= 
+
+// let lowercase; //! lowercase... not very good
+// let camelCasing; //* camel casing - standard
+// let PascalCasing; //? pascal casing - starts with a capital letter and has a capital for each new word - sometimes
+// let snake_casing_is_used_sometimes; //? snake casing - lower case letters with an underscore seperating each word...
+// let snake_casing = "cheese";
+// console.log(snake_casing)
+
+// depositing cash
+    // how would you like to deposit it?
+    // cheque
+    // cash
+// check balance
+    // different accounts
+    // mini-statement
+// statement
+// standing orders
+// enter pin
+    // validation... are you who you say you are?
+    // three attempts
+// change pin
+// money to mobile - top up
+// emergency cash
+// change language
+// change currency
+// print receipt
+// repeat the above...
