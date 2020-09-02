9bd938f11773498f1518822091977dd21c940dea
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-02 12:02:02 +0100

    if else statements created

---
+++ ifelse/index.js
@@ -0,0 +1,49 @@
+let favouriteSong = "Wrecking Ball";
+let favouriteArtist = "Miley Cyrus";
+let weather = "rain";
+// TODO: create a variable and then create an if statement with an if, else if and then an else.
+let dayOfTheWeek = "Monday";
+let age = 54;
+
+//* comparison operators:
+//* == Equal - checks the value
+//* === Strict Equal - checks the type && the value
+//* != Not equal - checks the value
+//* !== Strict Not Equal - checks the type && the value
+
+// if (age !== "55") { 
+//     //? is the value 55 && is the type a number?
+//     console.log("Your value and type match what I need.")
+// }
+// else if (age != "55") {
+//     console.log("Your value is all I need to check.")
+// }
+// else {
+//     console.log("I don't know what you did.")
+// }
+
+// if (weather == "rain") {
+//     console.log("Where is my umbrella?");
+// } 
+// else if (weather == "snowing") {
+//     console.log("Put a jacket on, fool.")
+// } 
+// else if (weather == "cloudy") {
+//     console.log("I don't know what to do.")
+// } 
+// else {
+//     console.log("Sandals. Get your sandals.")
+// }
+
+// ifttt 
+if (favouriteSong == "Wrecking Ball") {
+    console.log("You're very smart.");
+} else {
+    console.log("You owe me £100.")
+}
+
+// if (favouriteArtist == "Miley Cyrus") {
+//     console.log("You win £100.");
+// } else {
+//     console.log("You owe me £100.");
+// }
