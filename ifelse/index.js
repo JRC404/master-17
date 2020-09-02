31f1adee07a6d5c028e4e9326d23bf3d6f4642dc
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-02 12:34:57 +0100

    weather check with if...

--- ifelse/index.js
+++ ifelse/index.js
@@ -3,7 +3,96 @@ let favouriteArtist = "Miley Cyrus";
 let weather = "rain";
 // TODO: create a variable and then create an if statement with an if, else if and then an else.
 let dayOfTheWeek = "Monday";
-let age = 54;
+let age = 53;
+
+//* < > <= >= 
+//* && AND
+//* || OR
+
+// if (age <= 54) {
+//     console.log("You are allowed in without a discount.")
+// } else if (age > 54) {
+//     // do something here
+// } else {
+//     console.log("You are valid for a discount.")
+// }
+// DRY - don't repeat yourself
+// let info = "Hello, I am Jacob Reilly-Cooper. I'm 55 and ginger."
+// console.log(info)
+// console.log(info)
+// console.log(info)
+// let name = "Jacob"
+// let phone = "iPhone"
+// let info = `${name} ${phone}`
+// let info = name + phone
+// console.log(info)
+console.log(age, weather)
+if (age < 55 && weather == "sunny") {
+    // console.log(age, weather)
+    console.log("You are allowed in.")
+}
+else if (age < 55 || weather == "sunny") {
+    // console.log(age, weather)
+    console.log("You are also allowed in.")
+}
+else {
+    // console.log(age, weather)
+    console.log("You are not allowed in.")
+}
+
+
+
+
+// if (dayOfTheWeek == "Monday") {
+//     // do something
+//     console.log("Alarm")
+// }
+// else if (dayOfTheWeek == "Tuesday") {
+//     // do something
+//     console.log("Alarm")
+// }
+// else if (dayOfTheWeek == "Wednesday") {
+//     // do something
+//     console.log("Alarm")
+// }
+// else if (dayOfTheWeek == "Thursday") {
+//     // do something
+//     console.log("Alarm")
+// }
+// else if (dayOfTheWeek == "Friday") {
+//     // do something
+//     console.log("Alarm")
+// }
+// else if (dayOfTheWeek == "Saturday") {
+//     // do something
+//     console.log("No alarm")
+// }
+// else if (dayOfTheWeek == "Sunday") {
+//     console.log("No alarm")
+//     // do something
+// }
+
+// let name = "David"
+// if (condition1) {
+//     // do something
+// }
+// else if (condition2) {
+//     // do something
+// }
+// else {
+//     // captures everything else
+// }
+
+// if (dayOfTheWeek == "Monday") {
+//     // do something
+//     console.log("Woo, Monday!")
+// }
+// else if (dayOfTheWeek == "Tuesday") {
+//     // do something
+// }
+// else {
+//     console.log("Hi.")
+// }
 
 //* comparison operators:
 //* == Equal - checks the value
@@ -36,11 +125,11 @@ let age = 54;
 // }
 
 // ifttt 
-if (favouriteSong == "Wrecking Ball") {
-    console.log("You're very smart.");
-} else {
-    console.log("You owe me £100.")
-}
+// if (favouriteSong == "Wrecking Ball") {
+//     console.log("You're very smart.");
+// } else {
+//     console.log("You owe me £100.")
+// }
 
 // if (favouriteArtist == "Miley Cyrus") {
 //     console.log("You win £100.");
