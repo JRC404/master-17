e57cff6c64a0175c4b56dd5d0132ab479ad92371
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-02 09:56:42 +0100

    car journey variables created

---
+++ variables/journey.js
@@ -0,0 +1,16 @@
+let weather = "Rain";
+let umbrella;
+let traffic;
+let route;
+let modeOfTransport;
+let time;
+let averageSpeed;
+let speed;
+let currentLocation;
+let vehicle;
+let virusCarriers;
+
+
+const startingLocation = "Home";
+const EndLocation = "Home";
+console.log(startingLocation)
