8aa46b4edbe8b7b00692bb3d5718bd162d1950d7
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-07 11:59:46 +0100

    sayGoodbye method added

--- objects/person.js
+++ objects/person.js
@@ -6,7 +6,12 @@ const person =  {
     faveColour: 'Orange',
     sayHi () {
         console.log(`Hello my name is ${this.name}`);
+    },
+    sayGoodbye() {
+        console.log(`Goodbye, ${this.name}`);
     }
+
 }
 
 person.sayHi()
+person.sayGoodbye()
