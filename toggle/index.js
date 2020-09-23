47f08803158de8d45e2bd3a3d24b962d93b766dd
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-09-23 12:27:18 +0100

    toggle created. What is happening here?

---
+++ toggle/index.js
@@ -0,0 +1,27 @@
+let lightOn = true;
+
+const lightSwitch = () => {
+    if (lightOn) {
+        console.log("Light is on.")
+        lightOn = false;
+    }
+    else {
+        console.log("Light is off.")
+        lightOn = true;
+    }
+}
+
+lightSwitch()
+lightSwitch()
+lightSwitch()
+
+let playerOne = true;
+
+const playerSwitch = () => {
+    // if playerOne {
+        // target the playerOne text content
+    //}
+    // else  {
+        // target the playerTwo text content
+    //}
+}
